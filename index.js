972451c2e8ee87234690af75580ca781dfb0a7c0
Egor <egor2000sleptsov@gmail.com>
2021-09-06 19:17:31 +1000

    start 2

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require('express')
 const db = require('./db')
 const router = require('./router')
 const listOfStatus = require('./listOfStatus')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 const port = process.env.PORT || 5000
 const app = express()
 
+//for parsing the body of request
 app.use(express.urlencoded())
 app.use(express.json())
 
 //Router
 app.use(router)
 
+//define global objects
 global.db = db
 global.listOfStatus = listOfStatus
 
